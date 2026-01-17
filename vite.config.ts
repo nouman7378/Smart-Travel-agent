5dbc9d943c14fe141a055b8f21387c6f13390bbb
nouman7378 <noumanashraf2121@gmail.com>
2026-01-17 22:50:17 +0500

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(process.cwd(), './src'),
     },
   },
   build: {
